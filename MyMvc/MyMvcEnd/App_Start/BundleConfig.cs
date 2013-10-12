38a731debd7483a658aaa403a12af424de72a5f7
hp-renhuozhu <ren-huo.zhu@hp.com>
2013-10-12 13:16:08 +0800

    change the min.js to .js

---
+++ MyMvc/MyMvcEnd/App_Start/BundleConfig.cs
@@ -0,0 +1,32 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace MyMvcEnd
+{
+    public class BundleConfig
+    {
+        // 有关 Bundling 的详细信息，请访问 http://go.microsoft.com/fwlink/?LinkId=254725
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/login").Include(
+                        "~/Scripts/jquery-1.7.1.js",
+                        "~/Scripts/jquery.easyui.js",
+                        "~/Scripts/jquery.validate.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/layout").Include(
+                        "~/Scripts/jquery-1.7.1.js",
+                        "~/Scripts/jquery.unobtrusive-ajax.js",
+                        "~/Scripts/jquery.easyui.js",
+                        "~/Scripts/jquery.validate.js",
+                        "~/Scripts/dealmenu.js"));
+
+            bundles.Add(new StyleBundle("~/Content/login/css").Include(
+                        "~/Content/themes/default/easyui.css",
+                        "~/Content/themes/icon.css"));
+            bundles.Add(new StyleBundle("~/Content/layout/css").Include(
+                      "~/Content/themes/default/easyui.css",
+                      "~/Content/themes/icon.css",
+                      "~/Content/themes/demo.css"));
+        }
+    }
+}
